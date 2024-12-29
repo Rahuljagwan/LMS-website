75c61721daab26865db0b02f67976dd2c68fef87
Jagwan Rahul RajendraSingh <90298794+Rahuljagwan@users.noreply.github.com>
2024-12-29 12:00:00 +0530

    Update Course.jsx

--- client/src/pages/student/Course.jsx
+++ client/src/pages/student/Course.jsx
@@ -10,21 +10,21 @@ const Course = ({ course }) => {
       <Card className="overflow-hidden rounded-lg dark:bg-gray-800 bg-white shadow-lg hover:shadow-2xl transform hover:scale-105 transition-all duration-300">
         <div className="relative">
           <img
-            src={course.courseThumbnail}
+            src={course?.courseThumbnail}
             alt="course"
             className="w-full h-36 object-cover rounded-t-lg"
           />
         </div>
         <CardContent className="px-5 py-4 space-y-3">
           <h1 className="hover:underline font-bold text-lg truncate">
-            {course.courseTitle}
+            {course?.courseTitle}
           </h1>
           <div className="flex items-center justify-between">
             <div className="flex items-center gap-3">
               <Avatar className="h-8 w-8 ">
                 <AvatarImage
                   src={
-                    course.creator?.photoUrl || "https://github.com/shadcn.png"
+                    course?.creator?.photoUrl || "https://github.com/shadcn.png"
                   }
                   alt="@shadcn"
                 />
@@ -37,11 +37,11 @@ const Course = ({ course }) => {
                 "bg-teal-600 text-white px-2 py-1 text-xs rounded-full mt-1"
               }
             >
-              {course.courseLevel}
+              {course?.courseLevel}
             </Badge>
           </div>
           <div className="text-lg font-bold">
-            <span>₹{course.coursePrice}</span>
+            <span>₹{course?.coursePrice}</span>
           </div>
         </CardContent>
       </Card>
