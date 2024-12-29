bb81caaa75879fe041380594f999103083d51f3e
Jagwan Rahul RajendraSingh <90298794+Rahuljagwan@users.noreply.github.com>
2024-12-29 12:07:45 +0530

    Update MyLearning.jsx

--- client/src/pages/student/MyLearning.jsx
+++ client/src/pages/student/MyLearning.jsx
@@ -5,7 +5,7 @@ import { useLoadUserQuery } from "@/features/api/authApi";
 const MyLearning = () => {
   const { data, isLoading } = useLoadUserQuery();
 
-  const myLearning = data?.user.enrolledCourses || [];
+  const myLearning = data?.user?.enrolledCourses || [];
   return (
     <div className="max-w-4xl mx-auto my-10 px-4 md:px-0 pt-16">
       <h1 className="font-bold text-2xl ">MY LEARNING</h1>
